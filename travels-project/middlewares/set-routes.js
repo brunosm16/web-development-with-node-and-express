e2407faa72d6533b616368ab1c42198af384d32a
brunosm16 <brunoskr23@gmail.com>
2023-06-30 11:13:37 -0300

    feat: 🚀 add about and home routes

--- travels-project/middlewares/set-routes.js
+++ travels-project/middlewares/set-routes.js
@@ -1,22 +1,39 @@
-const { STATUS_CODE_400, STATUS_CODE_500 } = require('../constants/http-status-codes');
+const {
+	STATUS_CODE_400,
+	STATUS_CODE_500,
+	STATUS_CODE_200,
+} = require('../constants/http-status-codes');
 
 const NOT_FOUND_MESSAGE = '404 - Not found';
 const INTERNAL_SERVER_ERROR_MESSAGE = '500 - Internal Server Error';
+const HOMEPAGE_MESSAGE = 'Welcome to our travels agency';
+const ABOUT_PAGE_MESSAGE = 'About our travels agency';
 const TYPE_TEXT_PLAIN = 'text/plain';
+const API_PREFIX = '/travels';
+
+const sendResponse = (type, status, message, res) => {
+	if (!res) throw new Error('Response was not provided');
+
+	res.type(type);
+	res.status(status);
+	res.end(message);
+};
 
 const setRoutes = (app) => {
-	app.use((req, res) => {
-		res.type(TYPE_TEXT_PLAIN);
-		res.status(STATUS_CODE_400);
-		res.end(NOT_FOUND_MESSAGE);
-	});
+	app.get(`${API_PREFIX}/`, (req, res) =>
+		sendResponse(TYPE_TEXT_PLAIN, STATUS_CODE_200, HOMEPAGE_MESSAGE, res)
+	);
+
+	app.get(`${API_PREFIX}/about`, (req, res) =>
+		sendResponse(TYPE_TEXT_PLAIN, STATUS_CODE_200, ABOUT_PAGE_MESSAGE, res)
+	);
+
+	app.use((req, res) => sendResponse(TYPE_TEXT_PLAIN, STATUS_CODE_400, NOT_FOUND_MESSAGE, res));
 
 	// eslint-disable-next-line no-unused-vars
-	app.use((err, req, res, next) => {
-		res.type(TYPE_TEXT_PLAIN);
-		res.status(STATUS_CODE_500);
-		res.end(INTERNAL_SERVER_ERROR_MESSAGE);
-	});
+	app.use((err, req, res, next) =>
+		sendResponse(TYPE_TEXT_PLAIN, STATUS_CODE_500, INTERNAL_SERVER_ERROR_MESSAGE, res)
+	);
 };
 
 module.exports = setRoutes;
