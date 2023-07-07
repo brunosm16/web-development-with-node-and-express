c0c39d5385e14b1a5323fc6179b0b70528c4b8aa
brunosm16 <brunoskr23@gmail.com>
2023-07-07 09:22:04 -0300

    refactor: 📝 reuse code and remove verbose code

--- travels-project/lib/route-handlers.js
+++ travels-project/lib/route-handlers.js
@@ -1,28 +1,26 @@
 const { INTERNAL_SERVER_ERROR_VIEW } = require('../constants/views-names');
-const { STATUS_CODE_200, STATUS_CODE_500 } = require('../constants/http-status-codes');
+const { STATUS_CODE_500 } = require('../constants/http-status-codes');
 
 const renderFallbackView = (res) => {
 	res.status(STATUS_CODE_500);
 	res.render(INTERNAL_SERVER_ERROR_VIEW);
 };
 
-const formatData = (dataObj) => {
+const formatObjData = (dataObj) => {
 	const isObject = typeof dataObj === 'object';
-	const nonEmptyObject = isObject && !!Object.keys(dataObj)?.length;
-	if (nonEmptyObject) return dataObj;
-	return {};
+	return isObject ? dataObj : {};
 };
 
 const hasEmptyMandatoryFields = (view, status) => !status || !view;
 
-const renderView = (req, res, view, status = STATUS_CODE_200, dataObj = {}) => {
+const renderView = (req, res, view, status, dataObj = {}) => {
 	if (hasEmptyMandatoryFields(view, status)) {
 		renderFallbackView(res);
 		return;
 	}
 
-	if (status) res.status(status);
-	res.render(view, formatData(dataObj));
+	res.status(status);
+	res.render(view, formatObjData(dataObj));
 };
 
 module.exports = renderView;
