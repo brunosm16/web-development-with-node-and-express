53ab1335a71631b7ff3659a5a67e3bf154b44755
brunosm16 <brunoskr23@gmail.com>
2023-07-07 10:01:34 -0300

    feat: 🚀 redirect to about-page on home-page

--- travels-project/app.js
+++ travels-project/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const setRoutes = require('./middlewares/set-routes');
 
-const PORT = process.env.PORT ?? 3000;
-
 const app = express();
 
 app.engine(
@@ -18,5 +16,4 @@ app.use(express.static(`${__dirname}/public`));
 
 setRoutes(app);
 
-// eslint-disable-next-line no-console
-app.listen(PORT, () => console.log(`Server running at port : ${PORT}`));
+module.exports = app;
