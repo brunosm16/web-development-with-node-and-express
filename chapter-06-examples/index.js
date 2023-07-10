8ccf78a0bf086f04f3be0aaa2f0b0877cd5ffefb
brunosm16 <brunoskr23@gmail.com>
2023-07-10 15:12:12 -0300

    feat: 🚀 return json

--- chapter-06-examples/index.js
+++ chapter-06-examples/index.js
@@ -5,6 +5,7 @@ const formatRequestHeaders = require('./utils/format-request-headers');
 const retrieveInfoData = require('./utils/retrieve-info-data');
 const setBodyParser = require('./middlewares/set-body-parser');
 const handlers = require('./handlers');
+const getMockProducts = require('./mocks/get-mock-products');
 
 const PORT = 3000;
 
@@ -28,6 +29,7 @@ app.get('/plain-text', (req, res) => {
 app.get('/display-form-data', (req, res) => res.render('display-form-data'));
 app.get('/error-handler', (req, res) => res.status(500).render('error-handler'));
 app.post('/process-form', handlers.processForm);
+app.get('/get-products', (req, res) => res.json(getMockProducts()));
 app.use((req, res) => res.status(404).render('404'));
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
--- chapter-06-examples/mocks/get-mock-products.js
+++ chapter-06-examples/mocks/get-mock-products.js
@@ -1,10 +1,12 @@
 const { faker } = require('@faker-js/faker');
 
 const getMockProducts = () => ({
+	id: faker.string.uuid(),
 	product: faker.commerce.product(),
 	productName: faker.commerce.productName(),
 	productMaterial: faker.commerce.productMaterial(),
 	productAdjective: faker.commerce.productAdjective(),
+	price: faker.commerce.price(),
 });
 
 module.exports = getMockProducts;
